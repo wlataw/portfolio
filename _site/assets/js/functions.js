79ba9266c25b4e4d041bc260c411ae28cedb1486
Wojtek <wlatawiec@outlook.com>
2016-12-10 16:53:50 +0100

    Video background on chrome fix

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
 
-    $('#wrapper').fullpage({
-    
+  $('#wrapper').fullpage({
+      verticalCentered: true,
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+      afterRender: function(){
+        //playing the video
+        $('video').get(0).play();
+      }
     });
 
 });
