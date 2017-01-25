245ac8d9ab106df759dd4d920caefe2447c5ccfb
Wojtek <wlatawiec@outlook.com>
2017-01-25 20:33:34 +0100

    youtube preview, video bg, scrolling, header

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,12 +1,10 @@
 $( document ).ready(function() {
 
   $('#wrapper').fullpage({
-      verticalCentered: true,
-      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
-      afterRender: function(){
-        //playing the video
-        $('video').get(0).play();
-      }
-    });
-
+    verticalCentered: true,
+    afterRender: function(){
+      //playing the video
+      $('video').get(0).play();
+    }
+  });
 });
