ebbbc14837d3bc162be330b27b33594fe250bf89
Wojtek <wlatawiec@outlook.com>
2017-02-06 00:51:07 +0100

    Add mobile links, before first release

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -6,16 +6,22 @@ $( document ).ready(function() {
     afterRender: function(){
       //playing the video
       $('video').get(0).play();
-      $('.my-slider').unslider();
     }
   });
 
-  $(function() { // Wait for page to finish loading.
+  $(function() {
       var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
           if(iOS) { // Is iOS.
               $("video#bgvid").addClass('displayNone');
               $("img#bgimg").removeClass('displayNone');
               $("figure").addClass('iosHeight');
+          }
+
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            $("a.openLity").addClass('displayNone');
+            $("a.noLity").removeClass('displayNone');
+            $("h2.onDesktop").addClass('displayNone');
+            $("h2.onMobile").removeClass('displayNone');
           }
   });
 });
